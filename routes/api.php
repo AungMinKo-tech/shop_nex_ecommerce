dda07e04ef7cf15dc9bf778b61213bde5cbc3311
Aung Min Ko <aungminko951357.com>
2026-01-14 14:53:15 +0630

    refactor:auth_middleware

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ Route::post('/v1/refresh', [AuthController::class, 'refresh']);
 Route::post('/v1/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::prefix('v1/')->group(function () {
-    Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::middleware(['auth:sanctum', 'Role.check:1'])->group(function () {
         Route::apiResource('users', UserController::class)->only('index', 'show', 'destroy');
         Route::get('/roles', [RoleController::class, 'index']);
         Route::apiResource('categories', CategoryController::class);
